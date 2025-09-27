bd4474d386e432200f8193890311bb189b376b52
Lucas Nguyen <duyquan1710@gmail.com>
2025-09-27 15:47:45 -0700

    Align website center

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, useLocation } from "react-router-dom";
 import Landing from "./pages/Landing";
 import WeatherPage from "./pages/Weather";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="app">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/weather" element={<WeatherPage />} />
-        </Routes>
-      </div>
+      <AppShell />
     </BrowserRouter>
+  );
+}
+
+function AppShell() {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+  return (
+    <div className={`app ${isHome ? "home" : ""}`}>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/weather" element={<WeatherPage />} />
+      </Routes>
+    </div>
   );
 }
 
